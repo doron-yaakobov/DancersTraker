d6733ebff4a2ed0163bc3d749f12895a76e8dafe
dorony <doronyaakobov@gmail.com>
2024-01-23 16:37:54 +0200

    CSRT based tracker.

--- OpenCV crush course.py
+++ OpenCV crush course.py
@@ -5,22 +5,11 @@ Time frame: 3:18 - 4:33
 
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import sys
-import os
-import matplotlib.animation as FuncAnimation
-import urllib
-
-
-#
-
-
-# def displayRectangle(frame, bbox):
-#     plt.figure()
 
 
 def crop_video(start_time_in_msec: int = 198000, end_time_in_msec: int = 273000,
-               src_file: str = 'data/LaLaLand_A_lovely_night_scene.mp4', dst_file: str = 'data/cropped_video'):
+               src_file: str = 'data/video/LaLaLand_A_lovely_night_scene.mp4',
+               dst_file: str = 'data/video/cropped_video'):
     # Open the video
     vs = cv2.VideoCapture(src_file)
     # Set the video writer
@@ -55,68 +44,78 @@ def crop_video(start_time_in_msec: int = 198000, end_time_in_msec: int = 273000,
 def drawRectangle(frame, bbox):
     p1 = (int(bbox[0]), int(bbox[1]))
     p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-    cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+    cv2.rectangle(frame, p1, p2, (0, 255, 0), 2, 1)
 
 
-# crop_video()
-# drawRectangle(frame, [740, 335, 198, 535])
+def canny_filter(bgr_frame, dtype: str = "uint8", brightness_factor: int = 50, contrast_factor: int = 1.3,
+                 blurring_area=(2, 2), canny_min: int = 130, canny_max: int = 140):
+    def adjust_brightness(bgr_frame, dtype, factor: int = brightness_factor):
+        matrix = np.ones(bgr_frame.shape, dtype=dtype) * factor
+        return cv2.add(bgr_frame, matrix)
+
+    def adjust_contrast(bgr_frame, factor: int = contrast_factor):
+        matrix = np.ones(bgr_frame.shape) * factor
+        return np.uint8(np.clip(cv2.multiply(np.float64(bgr_frame), matrix), 0, 255))
+
+    bgr_frame = adjust_brightness(bgr_frame, dtype, brightness_factor)
+    bgr_frame = adjust_contrast(bgr_frame, contrast_factor)
+    gray_frame = cv2.cvtColor(bgr_frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_thresh_adp = cv2.adaptiveThreshold(gray_frame, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 4)
+    blur_frame = cv2.blur(gray_frame_thresh_adp, blurring_area)
+    return cv2.Canny(blur_frame, canny_min, canny_max)
 
 
-vs = cv2.VideoCapture('data/cropped_video.mp4')
-has_frame, frame = vs.read()
-bbox = [740, 335, 198, 535]
-tracker = cv2.legacy.TrackerCSRT().create()
+crop_video(start_time_in_msec=19000, dst_file="data/video/cropped_video(input)")
 
+if __name__ == "__main__":
+    vs = cv2.VideoCapture('data/video/cropped_video(input).mp4')
+    tracker = cv2.legacy.TrackerCSRT().create()
+    frame_width = int(vs.get(3))
+    frame_height = int(vs.get(4))
+    out = cv2.VideoWriter(f'{"data/video/cv2.trackerCSRT_output"}.mp4', cv2.VideoWriter.fourcc(*'XVID'), 10,
+                          (frame_width, frame_height))
 
-
-
-ok = tracker.init(frame, bbox)
-print(f"traker init stauts:{ok}")
-
-alive = True
-while alive:
+    # region init tracker
     has_frame, frame = vs.read()
-    if not has_frame:
-        break
+    bbox = [825, 375, 183, 415]
+    drawRectangle(frame, bbox)
+    cv2.imwrite("data/image/bboxed_first_dancer_frame.png", frame)
+    adjusted_frame = canny_filter(frame)
+    ok = tracker.init(adjusted_frame, bbox)
+    # cv2.imshow("frame", frame)
+    # cv2.waitKey(0)
+    # cv2.destroyWindow("frame")
+    # endregion
 
-    timer = cv2.getTickCount()
-    ok,bbox = tracker.update(frame)
-    fps = cv2.getTickFrequency()/(cv2.getTickCount()-timer)
-    if ok:
-        drawRectangle(frame, bbox)
+    print(f"traker init stauts:\t{ok}")
 
-    #
-    # # region change brightness and contrast
-    # # adjusting brightness:
-    # matrix = np.ones(frame.shape, dtype="uint8") * 50
-    # frame = cv2.add(frame, matrix)
-    # # adjusting contrast:
-    # matrix = np.ones(frame.shape) * 1.3
-    # frame = np.uint8(np.clip(cv2.multiply(np.float64(frame), matrix), 0, 255))
-    #
-    # # endregion
-    #
-    # gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # # gray_frame_thresh_adp = cv2.adaptiveThreshold(gray_frame, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 4)
-    # # gray_frame_thresh_adp = cv2.adaptiveThreshold(gray_frame, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9,2)
-    #
-    # # region canny filtering
-    # gray_frame = cv2.blur(gray_frame, (2, 2))
-    # gray_frame = cv2.Canny(gray_frame, 75, 80)
-    #
-    # # endregion
-    #
-    # # tracker = cv2.TrackerGOTURN.create()
-    #
-    # cv2.imshow("grayframe", gray_frame)
-    # # cv2.imshow("grayframe_thresh_adp", gray_frame_thresh_adp)
-    cv2.imshow("frame", frame)
-    pressed_key = cv2.waitKey(1)  # waits 1 mSec
-    if pressed_key == 27 or pressed_key == ord("q") or pressed_key == ord("Q"):  # Esc pressed
-        alive = False
+    alive = True
+    while alive:
+        has_frame, frame = vs.read()
+        if not has_frame:
+            print("No frame! breaking")
+            break
+        adjusted_frame = canny_filter(frame)
 
-# closing the video properly:
-vs.release()
-cv2.destroyAllWindows()
+        timer = cv2.getTickCount()
+        ok, bbox = tracker.update(adjusted_frame)
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
 
-exit()
+        if ok:
+            drawRectangle(frame, bbox)
+        else:
+            print("Failed to track.")
+
+        cv2.imshow("frame", frame)
+        out.write(frame)
+
+        pressed_key = cv2.waitKey(1)  # waits 1 mSec
+        if pressed_key == 27 or pressed_key == ord("q") or pressed_key == ord("Q"):  # Esc pressed
+            alive = False
+
+    # closing the video properly:
+    out.release()
+    vs.release()
+    cv2.destroyAllWindows()
+
+    exit()
